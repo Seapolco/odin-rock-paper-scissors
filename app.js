8263364d0430eadbefa9ca232a4a595c8157ab6c
Seapolco <seapolco@gmail.com>
2022-09-01 12:00:38 +0100

    Finished the required logic, following the description.
    I was hitting a few walls when I went back to follow the assignment step
    by step. I had the fundamental logic down, but I was getting stuck when
    trying to write the function which would play the game 5 times. I couldn't
    get my head round how I could use the playerchoice parameter multiple
    times, as it was a prompt, and I had declared at the top of the page.
    
    I tried using the prompt in the function as a parameter but this didn't
    work. Turned out it was mainly an issue of order. I had written the prompt
    at the top of the document after the computerchoice function. Seems obvious
    now, but because the parameter of the function had the same name as the
    variable, I thought I had to assign the value to the variable before I
    called the function.
    
    On to the next!

--- app.js
+++ app.js
@@ -1,41 +1,63 @@
-const rps = ['Rock', 'Paper', 'Scissors'];
-
-let randomIndex = () => {
-    return Math.floor(Math.random() * 3);
+function getComputerChoice () {
+    const rps = ['Rock', 'Paper', 'Scissors'];
+    let randomIndex = () => {
+        return Math.floor(Math.random() * 3);
+    };
+    let randomRPS = () => {
+        return rps[randomIndex()]
+    }
+    return randomRPS().toLowerCase();
 }
 
-let randomRPS = () => {
-    return rps[randomIndex()]
-}
+let playRound = (usersChoice, computersChoice) => {
 
-// let computersChoice = randomRPS();
-
-// let lusersChoice = prompt('Rock, Paper, Scissors?');
-
-let rockPaperScissors = () => {
-    let usersChoice = prompt('Rock, Paper, Scissors?');
-    let computersChoice = randomRPS();
-    if(computersChoice === "Rock" && usersChoice === "Paper") {
-               return console.log("You win!");
-            } else if (computersChoice === "Paper" && usersChoice === "Rock") {
-                return console.log("You lose!");
-            } else if (computersChoice === "Scissors" && usersChoice === "Rock") {
-                return console.log("You win!");
-            } else if (computersChoice === "Rock" && usersChoice === "Scissors") {
-                return console.log("You lose!");
-            } else if (computersChoice === "Paper" && usersChoice === "Scissors") {
-                return console.log("You win!");
-            } else if (computersChoice === "Scissors" && usersChoice === "Paper") {
-                return console.log("You lose!");
-            } else if (computersChoice === "Scissors" && usersChoice === "Scissors") {
-                return console.log("Draw!");
-            } else if (computersChoice === "Paper" && usersChoice === "Paper") {
-                return console.log("Draw!");
-            } else if (computersChoice === "Rock" && usersChoice === "Rock") {
-                return console.log("Draw!");
+    if(computersChoice === "rock" && usersChoice === "paper") {
+               return  "You win!";
+            } else if (computersChoice === "paper" && usersChoice === "rock") {
+                return "You lose!";
+            } else if (computersChoice === "scissors" && usersChoice === "rock") {
+                return "You win!";
+            } else if (computersChoice === "rock" && usersChoice === "scissors") {
+                return "You lose!";
+            } else if (computersChoice === "paper" && usersChoice === "scissors") {
+                return "You win!";
+            } else if (computersChoice === "scissors" && usersChoice === "paper") {
+                return "You lose!";
+            } else if (computersChoice === "scissors" && usersChoice === "scissors") {
+                return "Draw!";
+            } else if (computersChoice === "paper" && usersChoice === "paper") {
+                return "Draw!";
+            } else if (computersChoice === "rock" && usersChoice === "rock") {
+                return "Draw!";
             } 
+    }
+
+// console.log(playRound(usersChoice, computersChoice));
+
+let game = () => {
+
+    let wins = 0;
+    let loses = 0;
+    let draws = 0;
+
+    for(let i = 0; i < 5 ; i++) {
+        let usersChoice = prompt('Rock, Paper, Scissors?').toLowerCase();
+        let computersChoice = getComputerChoice();
+        let outcome = playRound(usersChoice,computersChoice)
+        outcome === "You win!" ? wins++ : outcome === "You lose!" ? loses++ : draws++;
+        console.log(outcome);
+    }
+
+    console.log(`W${wins}, L${loses}, D${draws}`)
+
+    if(wins > loses) {
+        console.log('Congratulations, you beat the computer!')
+    } else if(loses > wins) {
+        console.log('Commiserations, you lost to the computer!')
+    } else {
+        console.log("DRAW!")
+    }
 }
 
-for(let i = 0; i < 10; i++) {
-    rockPaperScissors()
-}
+game();
+
