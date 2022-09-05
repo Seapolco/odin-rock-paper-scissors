101199301b31d45bc904891ca32b9a4178cb8c7b
Seapolco <seapolco@gmail.com>
2022-09-05 18:59:42 +0100

    Finished the basic draft of the Rock Paper Scissors game.
    Basic UI and functionality complete. Yellow/Blue/Black color theme.
    Custome font and centered all the content. Need to add some images and
    some more fine polishing but this is a pretty good job considering.
    
    To-do
    
     - polish the sizing
     - add images etc for a more impressive experience
     - add button styling
     - experiment with transition
     - eventually go in ham and add custom images, transitions, animations etc
     for a super stylish UE.
    
    Please enter the commit message for your changes. Lines starting

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-const outcome = document.querySelector('.outcome');
-console.log(outcome);
+let fight = document.querySelector('.fight');
+let outcome = document.createElement('div');
+let finalResult = document.createElement('div');
+let message = document.createElement('div');
+
+outcome.classList.add("outcome");
+
+let runningScore = document.createElement('div');
+
+// Selecting the wins/draws/losses divs 
+
+let winsTracker = document.querySelector(".wins");
+let lossesTracker = document.querySelector(".losses");
+let drawsTracker = document.querySelector(".draws");
+
+console.log(winsTracker.textContent)
+
+let counter = 0;
+
+
+let wins = 0;
+let loses = 0;
+let draws = 0;
+
 
 
 function getComputerChoice () {
@@ -12,80 +34,214 @@ function getComputerChoice () {
     }
     return randomRPS().toLowerCase();
 }
+// let determineScore = (usersChoice, computersChoice) => {
 
-let playRound = (usersChoice, computersChoice) => {
 
-    if(computersChoice === "rock" && usersChoice === "paper") {
-               return  "You win!";
+//     if(computersChoice === "rock" && usersChoice === "paper") {
+//                return  "Round Won!";
+//             } else if (computersChoice === "paper" && usersChoice === "rock") {
+//                 return "Round Lost!";
+//             } else if (computersChoice === "scissors" && usersChoice === "rock") {
+//                 return "Round Won!";
+//             } else if (computersChoice === "rock" && usersChoice === "scissors") {
+//                 return "Round Lost!";
+//             } else if (computersChoice === "paper" && usersChoice === "scissors") {
+//                 return "Round Won!";
+//             } else if (computersChoice === "scissors" && usersChoice === "paper") {
+//                 return "Round Lost!";
+//             } else if (computersChoice === "scissors" && usersChoice === "scissors") {
+//                 return "Draw!";
+//             } else if (computersChoice === "paper" && usersChoice === "paper") {
+//                 return "Draw!";
+//             } else if (computersChoice === "rock" && usersChoice === "rock") {
+//                 return "Draw!";
+//             } 
+//     }
+
+
+let usersChoice;
+
+let buttons = document.querySelectorAll('button');
+
+// function setUsersChoice(e) {
+//     usersChoice = e.target.textContent;
+//     console.log(usersChoice)
+// }
+
+function resultTracker(roundResult) {
+
+}
+
+
+function playRound(e) {
+
+
+
+
+
+    if(counter < 5) {
+        counter += 1;
+    }
+    if(counter === 1) {
+        fight.style.color = "#fcc729";
+        outcome.style.color = "#fcc729";
+    }
+    // if(counter === 5) {
+
+    // }
+    console.log(counter)
+    let computersTurn = getComputerChoice();
+    usersChoice = e.target.textContent.toLowerCase();
+    console.log(`CC${computersTurn} UC${usersChoice}`);
+    
+    let determineScore = (usersChoice, computersChoice) => {
+
+
+            if(computersChoice === "rock" && usersChoice === "paper") {
+                wins++;
+                return  "Round Won!";
             } else if (computersChoice === "paper" && usersChoice === "rock") {
-                return "You lose!";
+                loses++;
+                return "Round Lost!";
             } else if (computersChoice === "scissors" && usersChoice === "rock") {
-                return "You win!";
+                wins++;
+                return "Round Won!";
             } else if (computersChoice === "rock" && usersChoice === "scissors") {
-                return "You lose!";
+                loses++;
+                return "Round Lost!";
             } else if (computersChoice === "paper" && usersChoice === "scissors") {
-                return "You win!";
+                wins++;
+                return "Round Won!";
             } else if (computersChoice === "scissors" && usersChoice === "paper") {
-                return "You lose!";
+                loses++;
+                return "Round Lost!";
             } else if (computersChoice === "scissors" && usersChoice === "scissors") {
+                draws++;
                 return "Draw!";
             } else if (computersChoice === "paper" && usersChoice === "paper") {
+                draws++;
                 return "Draw!";
             } else if (computersChoice === "rock" && usersChoice === "rock") {
+                draws++;
                 return "Draw!";
             } 
     }
 
-// console.log(playRound(usersChoice, computersChoice));
+    let roundScore = determineScore(usersChoice, computersTurn);
+    console.log(`Roundscore:${roundScore}`)
 
-let game = () => {
+    // roundScore === "Round Won!" ? wins++ : roundScore === "Round Lost!" ? 
+    //     loses++ : draws++;
 
-    let wins = 0;
-    let loses = 0;
-    let draws = 0;
+    console.log(`W${wins} L${loses} D${draws}`)
+    outcome.textContent = roundScore;
+    fight.appendChild(outcome);
 
-    const message = document.createElement('div');
-    const total = document.createElement('div');
+    winsTracker.textContent = wins;
+    lossesTracker.textContent = loses;
+    drawsTracker.textContent = draws;
 
-    for(let i = 0; i < 5 ; i++) {
-        let usersChoice = prompt('Rock, Paper, Scissors?').toLowerCase();
-        let computersChoice = getComputerChoice();
-        let outcome = playRound(usersChoice,computersChoice)
-        outcome === "You win!" ? wins++ : outcome === "You lose!" ? loses++ : draws++;
-        console.log(outcome);
-    }
+    // runningScore.textContent = `W${wins} L${loses} D${draws}`;
 
-    console.log(`W${wins}, L${loses}, D${draws}`)
+    // fight.appendChild(runningScore);
 
-    if(wins > loses) {
+    if(counter === 5) {
+        if(wins > loses) {
         
-        console.log('Congratulations, you beat the computer!');
-        outcome.style.color = "Green";
-        outcome.textContent = "Victory!";
-        message.textContent = 'Congratulations, you beat the computer!';
-        outcome.appendChild(message);
-        total.textContent = `W${wins}, L${loses}, D${draws}`;
-        message.appendChild(total);
+            console.log('Congratulations, Round beat the computer!');
+            outcome.style.color = "Green";
+            outcome.textContent = "Victory!";
+            message.textContent = 'Congratulations, you beat the computer!';
+            outcome.appendChild(message);
+            // total.textContent = `W${wins}, L${loses}, D${draws}`;
+            // message.appendChild(total);
+    
+        } else if(loses > wins) {
+            console.log('Commiserations, Round lost to the computer!');
+            outcome.style.color = "Red";
+            outcome.textContent = "Defeat!";
+            message.textContent = 'Commiserations, you lost to the computer!';
+            outcome.appendChild(message);
+            // total.textContent = `W${wins}, L${loses}, D${draws}`;
+            // message.appendChild(total);
+        } else {
+            console.log("DRAW!");
+            outcome.style.color = "Blue";
+            outcome.textContent = "Draw!";
+            message.textContent = "No winners or losers today. Play Again?";
+            outcome.appendChild(message);
+            // total.textContent = `W${wins}, L${loses}, D${draws}`;
+            // message.appendChild(total);
+        }
+        counter = 0;
+        wins = 0;
+        loses = 0;
+        draws = 0;
 
-    } else if(loses > wins) {
-        console.log('Commiserations, you lost to the computer!');
-        outcome.style.color = "Red";
-        outcome.textContent = "Defeat!";
-        message.textContent = 'Commiserations, you lost to the computer!';
-        outcome.appendChild(message);
-        total.textContent = `W${wins}, L${loses}, D${draws}`;
-        message.appendChild(total);
-    } else {
-        console.log("DRAW!");
-        outcome.style.color = "Blue";
-        outcome.textContent = "Draw!";
-        message.textContent = 'Congratulations and commiserations, you both won and lost!';
-        outcome.appendChild(message);
-        total.textContent = `W${wins}, L${loses}, D${draws}`;
-        message.appendChild(total);
+
     }
 
 }
 
-game();
+buttons.forEach((btn) => {
+    btn.addEventListener('click', playRound)
+});
+
+
+
+
+
+
+// console.log(playRound(usersChoice, computersChoice));
+
+// let game = () => {
+
+//     let wins = 0;
+//     let loses = 0;
+//     let draws = 0;
+
+//     const message = document.createElement('div');
+//     const total = document.createElement('div');
+
+    // for(let i = 0; i < 5 ; i++) {
+    //     let usersSelection = usersChoice;
+    //     let computersChoice = getComputerChoice();
+    //     let outcome = playRound(usersChoice,computersChoice)
+    //     outcome === "Round Won!" ? wins++ : outcome === "Round Lost!" ? loses++ : draws++;
+    //     console.log(outcome);
+    // }
+
+    //console.log(`W${wins}, L${loses}, D${draws}`)
+
+    // if(wins > loses) {
+        
+    //     console.log('Congratulations, Round beat the computer!');
+    //     outcome.style.color = "Green";
+    //     outcome.textContent = "Victory!";
+    //     message.textContent = 'Congratulations, Round beat the computer!';
+    //     outcome.appendChild(message);
+        // total.textContent = `W${wins}, L${loses}, D${draws}`;
+        // message.appendChild(total);
+
+    // } else if(loses > wins) {
+    //     console.log('Commiserations, Round lost to the computer!');
+    //     outcome.style.color = "Red";
+    //     outcome.textContent = "Defeat!";
+    //     message.textContent = 'Commiserations, Round lost to the computer!';
+    //     outcome.appendChild(message);
+        // total.textContent = `W${wins}, L${loses}, D${draws}`;
+        // message.appendChild(total);
+    // } else {
+    //     console.log("DRAW!");
+    //     outcome.style.color = "Blue";
+    //     outcome.textContent = "Draw!";
+    //     message.textContent = 'Congratulations and commiserations, Round both won and lost!';
+    //     outcome.appendChild(message);
+        // total.textContent = `W${wins}, L${loses}, D${draws}`;
+        // message.appendChild(total);
+    // }
+
+// }
+
+// game();
 
